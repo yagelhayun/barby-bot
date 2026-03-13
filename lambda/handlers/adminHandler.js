1279d24754cf72a3e850364e5e90aadba1500bec
Yagel Hayun <yagel1999@gmail.com>
2026-03-13 19:07:33 +0200

    add admin handler boilerplate

---
+++ lambda/handlers/adminHandler.js
@@ -0,0 +1,5 @@
+export const adminHandler = async (event, context) => {
+    console.log('Admin handler invoked');
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('Context:', JSON.stringify(context, null, 2));
+}
--- lambda/index.js
+++ lambda/index.js
@@ -1,8 +1,17 @@
 import 'dotenv/config';
+import { adminHandler } from './handlers/adminHandler.js';
 import { notificationsHandler } from './handlers/notificationsHandler.js';
 
 export const main = async (event, context) => {
-    return notificationsHandler(event, context);
+    if (event.source === "aws.events") {
+        return notificationsHandler(event, context);
+    }
+
+    if (event.version === "2.0") {
+        return adminHandler(event, context);
+    }
+
+    return { statusCode: 200, body: "ignored" }
 }
 
 if (process.env.NODE_ENV === 'development') {
