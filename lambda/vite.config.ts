4910a36943ecb8c4b90e284de907f911988c9bd7
Yagel Hayun <yagel1999@gmail.com>
2026-03-22 04:55:15 +0200

    fix: build configuration

--- lambda/vite.config.ts
+++ lambda/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve, isAbsolute } from 'path';
 
 export default defineConfig({
     build: {
         lib: {
-            entry: 'src/index.ts',
+            entry: resolve(__dirname, 'src/index.ts'),
             formats: ['es'],
             fileName: () => 'index.js',
         },
         rollupOptions: {
-            external: (id) => !id.startsWith('.') && !id.startsWith('/'),
+            external: (id) => !isAbsolute(id) && !id.startsWith('.'),
         },
         outDir: 'dist',
         target: 'node24',
